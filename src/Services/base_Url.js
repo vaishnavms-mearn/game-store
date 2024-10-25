e0668d17a47d4def382272c5940d6dbd5cfa0550
vaishnavms9656 <vaishnavms9656@gmail.com>
2024-10-25 14:05:17 +0530

    page commit

--- src/Services/base_Url.js
+++ src/Services/base_Url.js
@@ -1,1 +1,1 @@
-export const base_Url="https://game-store-backend-d1xq-ltb0r4l4t-vaishnavms-mearns-projects.vercel.app/"
+export const base_Url="https://game-store-backend-d1xq-ltb0r4l4t-vaishnavms-mearns-projects.vercel.app"
